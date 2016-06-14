d4f02eb74d443ed0767397e4fa00542068fe1440
karuzela <najmniejby@gmail.com>
2016-06-14 11:40:37 +0200

    title circle, media queries, small screen

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+function hover_main_circle (circle){
+    var wrapcontent = $(".wrapcontent");
+    var p = wrapcontent.find("p");
+    
+    circle.on("mouseenter", function(){
+        wrapcontent.html("<p>Scroll me!</p>");
+        p.addClass("scroll_me");
+    })
+}
+
+function leave_main_circle (circle){
+    var wrapcontent = $(".wrapcontent");
+    var p = wrapcontent.find("p");
+    
+    circle.on("mouseleave", function(){
+        wrapcontent.html("<p>foo, bar</p><p>or not?</p>");
+        p.removeClass("scroll_me");;
+    })
+}
+
+$(document).ready(function(){
+   hover_main_circle($(".circle"));
+    leave_main_circle($(".circle"));
+});
