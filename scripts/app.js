8e274ac8ad142f505efed9b85538fa1a32298d2e
karuzela <najmniejby@gmail.com>
2016-06-17 12:26:31 +0200

    foo button and left arrow key both work

--- scripts/app.js
+++ scripts/app.js
@@ -1,119 +1,85 @@
-/* pojawia się ekran do gry */
-function prepareTheGameScreen(){
-    var game_screen = $("div.game_container");
-    
-    $("div.ready_container").remove();
-    game_screen.animate({
-            top: "0vh"}, 3000);
+var gamePoints = {
+    points: 0,
+    accuracy: 0,
+    clicks: 0,
+    random_number: null,
+    isActive: false
 }
 
-/* efekt pisania na maszynie */
-function typeWriting (newString){
-    var comment = $("#span_ready");
-    var typeWriting = new TypeWriting({
-            targetElement: comment [0],
-            inputString: newString,
-            typing_interval: 130,
-            blink_interval: '1s',
-            cursor_color: '#9c1b82',
-        });
-}
-
-/* animacja odjazdu ekranu ready */
-function removeReadyScreen (delay){
-    var ready_screen = $("div.ready_container");
-    
-    ready_screen.delay(delay).animate({
-            top: "-100vh"}, 1000, function(){
-                prepareTheGameScreen();
-            });
-}
-
-/* po naciśnięciu buttona YES ukrywa się ekran z pytaniem "ready?" i pojawia się ekran do gry */
-function clickButtonYes(){
-    var button_yes = $(".yes_button");
-    var heart_container = $(".heart-Container");
-    var comment = $("#span_ready");
-    
-    button_yes.on("click", function(){
-        if (heart_container.hasClass("broken")){
-            heart_container.removeClass("broken");
-            removeReadyScreen(1100);
-        }  
-        else if( comment.hasClass("eyes")) {
-            typeWriting("(@_@)");
-            removeReadyScreen(1200);
-        }
-        else {
-            removeReadyScreen(0);
-        }
-    })
-}
-
-/* po naciśnięciu buttona MAYBE pojawia się zaskoczona minka */
-function clickButtonMaybe(){
-    var button_maybe = $(".maybe_button");
-    var comment = $("#span_ready");
-    var heart = $("#heart");
-    
-    button_maybe.on("click", function(){
-        heart.addClass("heart_hide");
-        heart.find('.heart-Container').removeClass('broken');
-        comment.removeClass("span_ready_hide");
-        comment.addClass('eyes');
-        typeWriting("(0_0)");
-    })
-}
-
-/* po naciśnięciu buttona NO pojawia się pęknięte serce */
-function clickButtonNo(){
-    var button_no = $(".no_button");
-
-    button_no.click(function(){
-        var heart = $("#heart");
-        var comment = $("#span_ready");
-    
-        comment.addClass("span_ready_hide");
-        heart.find('.heart-Container').removeClass('broken');
-        heart.removeClass("heart_hide");
-        setTimeout(function() {
-            heart.find('.heart-Container').addClass('broken');
-        }, 800);
-    })
-        
-}
-
-/* serce musi być ukryte, pojawia się dopiero w momencie wciśnięcia buttona NO */
-function hideHeart(){
-    $("#heart").toggleClass("heart_hide");
-}
-
-function activeButtons(){
+function startGame(){
+    gamePoints.points = 0;
+    gamePoints.accuracy = 0;
+    gamePoints.clicks = 0;
+    var counter_value = $("#counter_value");
+    var start = $(".game_screen_wrapcontent_ready");
+    var buttons = $(".game_screen_wrapcontent_buttons");
     var foo_button = $(".foo_button");
     var bar_button = $(".bar_button");
     var foobar_button = $(".foobar_button");
     var not_button = $(".not_button");
+  
     
-//    foo_button.on("click", function({
-//        if ()
-//    }))
+    start.addClass("start").html("START");
+    start.on("click", function(){
+        gamePoints.random_number = getRandomNumber();
+        showRandomNumber( gamePoints.random_number);
+        start.removeClass("start");
+        gamePoints.isActive = true;
+        // uruchamia timer //
+        $('.timer').startTimer({
+            onComplete: function(element){
+                gamePoints.isActive = false;
+                showGameOver(gamePoints.points, gamePoints.accuracy, gamePoints.clicks);
+            }
+        });
+        start.off("click");
+    });
     
+   
+        foo_button.on("click", function(){
+             if (gamePoints.isActive) {
+                    isFoo();
+             }
+        })
 }
 
-function showGameOver(){
+function isFoo (){
+    var counter_value = $("#counter_value");
+     
+     if ( gamePoints.random_number % 3 === 0){
+            gamePoints.points = gamePoints.points + 10;
+            gamePoints.accuracy++;
+            console.log("333!!!");
+        }
+        gamePoints.random_number = getRandomNumber();
+        showRandomNumber( gamePoints.random_number);
+        gamePoints.clicks++;
+        counter_value.html(gamePoints.points); 
+ }
+
+function getRandomNumber(){
+    return Math.ceil(Math.random() * 100); 
+}
+
+function showRandomNumber(random_number){
+    var start = $(".game_screen_wrapcontent_ready");
+    
+    start.removeClass("start");
+    start.html(random_number);
+// animacja numerów
+    transform_number($('.counter'), 30, 'fixed_width');
+    return random_number;
+}
+
+function showGameOver(points, accuracy, clicks){
     var start = $(".game_screen_wrapcontent_ready");
     var buttons = $(".game_screen_wrapcontent_buttons");
-    var points = 0;
-    var accuracy = 0;
-//    var points_count = "points:" + points;
-//    var points_copy = $("<p>points_count")
     
     start.removeClass("start");
     buttons.addClass("gameover_buttons");
     start.addClass("gameover");
-   
     start.html(function (){
-        return "<span class='gameover_copy'>game over</span><span class='gameover_points_accuracy'>points: " + points + "</span><span class='gameover_points_accuracy'>accuracy: " + accuracy + "</span>"
+        return "<div class='score'><p class='gameover_copy'>game over</p><p class='gameover_points_accuracy'>points: " + points + "</p><p class='gameover_points_accuracy'>accuracy: " + accuracy + "/" + clicks + "</p><button class='play_again'>play again</button></div>"
     });
     start.animate({
         width: "35vh",
@@ -122,39 +88,36 @@ function showGameOver(){
         },200)
 }
 
-function startGame(){
-    var start = $(".game_screen_wrapcontent_ready");
-    var buttons = $(".game_screen_wrapcontent_buttons");
+function playAgain(){
+    var button_play_again = $(".play_again");
     
-    start.addClass("start").html("START");
-    start.on("click", function(){
-        showRandomNumber(getRandomNumber);
-        start.removeClass("start");
-// uruchamia timer //
-        $('.timer').startTimer({
-            onComplete: function(element){
-                showGameOver();
-            }
-        });
-        start.off("click");
+    button_play_again.on("click", function(){
+        console.log("hello");
+        startGame();
     })
 }
 
-function getRandomNumber(){
-    var random_number = Math.ceil(Math.random() * 100);
-    return random_number;
-}
-
-function showRandomNumber(getRandomNumber){
-//    var random_number = getRandomNumber();
-    var start = $(".game_screen_wrapcontent_ready");
-    
-    start.removeClass("start");
-    start.html(getRandomNumber);
-    transform_number($('.counter'), 30, 'fixed_width');
-}
-
 $(document).ready(function(){
+ 
+    document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            if (gamePoints.isActive) {
+                    isFoo();
+             }
+            break;
+        case 38:
+            alert('up');
+            break;
+        case 39:
+            alert('right');
+            break;
+        case 40:
+            alert('down');
+            break;
+        }
+    };
+    
     hover_main_circle($(".main_circle"));
     leave_main_circle($(".main_circle"));
     animation_second_rule();
@@ -164,6 +127,7 @@ $(document).ready(function(){
     clickButtonNo();
     hideHeart();
     startGame();
+    playAgain();
     
     $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
         {
---
+++ scripts/ready_screen.js
@@ -0,0 +1,88 @@
+/* pojawia się ekran do gry */
+function prepareTheGameScreen(){
+    var game_screen = $("div.game_container");
+    
+    $("div.ready_container").remove();
+    game_screen.animate({
+            top: "0vh"}, 3000);
+}
+
+/* efekt pisania na maszynie */
+function typeWriting (newString){
+    var comment = $("#span_ready");
+    var typeWriting = new TypeWriting({
+            targetElement: comment [0],
+            inputString: newString,
+            typing_interval: 130,
+            blink_interval: '1s',
+            cursor_color: '#9c1b82',
+        });
+}
+
+/* animacja odjazdu ekranu ready */
+function removeReadyScreen (delay){
+    var ready_screen = $("div.ready_container");
+    
+    ready_screen.delay(delay).animate({
+            top: "-100vh"}, 1000, function(){
+                prepareTheGameScreen();
+            });
+}
+
+/* po naciśnięciu buttona YES ukrywa się ekran z pytaniem "ready?" i pojawia się ekran do gry */
+function clickButtonYes(){
+    var button_yes = $(".yes_button");
+    var heart_container = $(".heart-Container");
+    var comment = $("#span_ready");
+    
+    button_yes.on("click", function(){
+        if (heart_container.hasClass("broken")){
+            heart_container.removeClass("broken");
+            removeReadyScreen(1100);
+        }  
+        else if( comment.hasClass("eyes")) {
+            typeWriting("(@_@)");
+            removeReadyScreen(1200);
+        }
+        else {
+            removeReadyScreen(0);
+        }
+    })
+}
+
+/* po naciśnięciu buttona MAYBE pojawia się zaskoczona minka */
+function clickButtonMaybe(){
+    var button_maybe = $(".maybe_button");
+    var comment = $("#span_ready");
+    var heart = $("#heart");
+    
+    button_maybe.on("click", function(){
+        heart.addClass("heart_hide");
+        heart.find('.heart-Container').removeClass('broken');
+        comment.removeClass("span_ready_hide");
+        comment.addClass('eyes');
+        typeWriting("(0_0)");
+    })
+}
+
+/* po naciśnięciu buttona NO pojawia się pęknięte serce */
+function clickButtonNo(){
+    var button_no = $(".no_button");
+
+    button_no.click(function(){
+        var heart = $("#heart");
+        var comment = $("#span_ready");
+    
+        comment.addClass("span_ready_hide");
+        heart.find('.heart-Container').removeClass('broken');
+        heart.removeClass("heart_hide");
+        setTimeout(function() {
+            heart.find('.heart-Container').addClass('broken');
+        }, 800);
+    })    
+}
+
+/* serce musi być ukryte, pojawia się dopiero w momencie wciśnięcia buttona NO */
+function hideHeart(){
+    $("#heart").toggleClass("heart_hide");
+}
